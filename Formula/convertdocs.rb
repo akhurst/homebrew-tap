7c207a6aeea126c7ac5f0a7a63414b3c1ef72d21
Allen Hurst <allenhurst@gmail.com>
2025-03-22 12:26:06 -0500

    Fixing github URLs

--- Formula/convertdocs.rb
+++ Formula/convertdocs.rb
@@ -1,7 +1,7 @@
 class Convertdocs < Formula
   desc "CLI tool to convert Word documents to PDFs using LibreOffice"
-  homepage "https://github.com/yourusername/convertdocs"
-  url "https://github.com/yourusername/convertdocs/releases/download/v1.0.0/convertdocs"
+  homepage "https://github.com/akhurst/convertdocs"
+  url "https://github.com/akhurst/convertdocs/releases/download/v1/convertdocs"
   sha256 "5036f06249bcc54db28c7b71ca9003bb56452ebfac61fac56c013b30ae05ec78"
   version "1.0.0"
 
